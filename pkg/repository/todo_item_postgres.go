d2d4c7f7b1e8355b3391c51ce591d8ec17a66544
Лёва Нерсесян <lyova123454321@gmail.com>
2025-01-03 20:20:31 +0300

    cr and get item

---
+++ pkg/repository/todo_item_postgres.go
@@ -0,0 +1,51 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/Snake1-1eyes/todo-app"
+	"github.com/jmoiron/sqlx"
+)
+
+type TodoItemPostgres struct {
+	db *sqlx.DB
+}
+
+func NewTodoItemPostgres(db *sqlx.DB) *TodoItemPostgres {
+	return &TodoItemPostgres{db: db}
+}
+
+func (r *TodoItemPostgres) Create(listId int, item todo.TodoItem) (int, error) {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return 0, err
+	}
+
+	var Itemid int
+	createItemQuery := fmt.Sprintf("INSERT INTO %s (title, description) VALUES ($1, $2) RETURNING id", todoItemsTable)
+
+	row := tx.QueryRow(createItemQuery, item.Title, item.Description)
+	if err := row.Scan(&Itemid); err != nil {
+		return 0, err
+	}
+
+	createListItemsQuery := fmt.Sprintf("INSERT INTO %s (list_id, item_id) VALUES ($1, $2)", listsItemsTable)
+	_, err = tx.Exec(createListItemsQuery, listId, Itemid)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	return Itemid, tx.Commit()
+}
+
+func (r *TodoItemPostgres) GetAll(userId, listId int) ([]todo.TodoItem, error) {
+	var items []todo.TodoItem
+	query := fmt.Sprintf("SELECT ti.id, ti.title, ti.description, ti.done FROM %s ti INNER JOIN %s li ON ti.id = li.item_id INNER JOIN %s ul ON li.list_id = ul.list_id WHERE ul.list_id=$1 AND ul.user_id=$2", todoItemsTable, listsItemsTable, usersListsTable)
+
+	if err := r.db.Select(&items, query, listId, userId); err != nil {
+		return nil, err
+	}
+
+	return items, nil
+}
